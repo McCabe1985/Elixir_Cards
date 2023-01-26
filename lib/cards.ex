bf4deaee3b9e4501bed98109a2d9673c38dbc87d
Patrick McCabe <pmccabe.dev@gmail.com>
2023-01-26 12:27:02 +0000

    carry on my wayward son, there'll be peace when you are done

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,15 +1,6 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
+  Provides methods for creating and handling a deck of cards
 
   """
   def create_deck do
@@ -32,4 +23,23 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary } -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
 end
